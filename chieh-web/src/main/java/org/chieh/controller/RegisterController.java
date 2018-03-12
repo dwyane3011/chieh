4c9b4618360ed4df26866301031d223938ab0333
dwyane3011 <dwyane3011@163.com>
2018-03-12 19:38:11 +0800

    登录注册

--- chieh-web/src/main/java/org/chieh/controller/RegisterController.java
+++ chieh-web/src/main/java/org/chieh/controller/RegisterController.java
@@ -2,6 +2,9 @@ package org.chieh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
 @RequestMapping("/register")
@@ -11,5 +14,15 @@ public class RegisterController extends BaseController {
 	public String index() {
 		logger.info("注册页面");
 		return "/register/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/register")
+	public JSONObject register() {
+		logger.info("注册成功");
+		JSONObject res = new JSONObject();
+		res.put("code", 0);
+		res.put("msg", "注册成功");
+		return res;
 	}
 }
--- chieh-web/src/main/webapp/WEB-INF/jsp/register/index.jsp
+++ chieh-web/src/main/webapp/WEB-INF/jsp/register/index.jsp
@@ -5,6 +5,12 @@
 <title>欢迎━(*｀∀´*)ノ亻! 注册</title>
 </head>
 <body>
-
+<div style="width: 100%; height: 80px; background: #fff;"></div>
+<div style="width: 100%; min-height: 510px; background: #fff; background-image: url(${pageContext.request.contextPath}/images/register/background.jpg);">
+    <form action="${pageContext.request.contextPath}/register/register" method="post">
+        <input type="submit" value="立即注册">
+    </form>
+</div>
+<div style="width: 100%; height: 80px; background: #fff;"></div>
 </body>
 </html>
